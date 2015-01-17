f985f9e304676f389361a65086214bd7faaeb1d1
aorura <hyponus@gmail.com>
2015-01-18 00:12:45 +0900

    add client id

--- Lesson_6/00_Conference_Central/src/main/webapp/js/app.js
+++ Lesson_6/00_Conference_Central/src/main/webapp/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'your-client-id',
+        CLIENT_ID: '877771652124-09emfe0m75sret2ku98fi0klrjm42ip9.apps.googleusercontent.com',
         SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
         signedIn: false
     };
